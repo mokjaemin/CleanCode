58f7f05e93d2c7e9c8f2433bb2c1e0ed93d6c572
JaeminMok <bamer@naver.com>
2024-02-12 15:55:35 +0900

    Member Unit Test Finish

--- src/test/java/UnitTest/Member/MemberDAOTest.java
+++ src/test/java/UnitTest/Member/MemberDAOTest.java
@@ -2,12 +2,18 @@ package UnitTest.Member;
 
 import DAO.Impl.MemberDAOImpl;
 import DAO.MemberDAO;
+import Data.DTO.Input.DeleteMember;
+import Data.DTO.Input.LoginMember;
+import Data.DTO.Input.Member;
 import Data.Entity.MemberEntity;
 import DataBase.Impl.MemberDBImpl;
 import DataBase.MemberDB;
 import TestUtil.MemberTestUtil;
 import org.junit.jupiter.api.Test;
 import Exception.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.mockito.Mockito.*;
 
@@ -58,7 +64,107 @@ public class MemberDAOTest {
         verify(memberDB, times(1)).registerMemberEntity(memberEntity);
     }
 
+    @Test
+    public void isMemberLoginValidSuccess(){
+        // Given
+        LoginMember loginMember = MemberTestUtil.givenLoginMember();
+        when(memberDB.getRegisteredLoginMember(loginMember)).thenReturn(loginMember);
 
+        // When
+        boolean response = memberDAO.isMemberLoginValid(loginMember);
+
+        // Then
+        assert response;
+    }
+
+    @Test
+    public void isMemberLoginValidFail_NoRegisteredIDException(){
+        // Given
+        LoginMember loginMember = MemberTestUtil.givenLoginMember();
+        when(memberDB.getRegisteredLoginMember(loginMember)).thenReturn(null);
+
+        try {
+            // When
+            memberDAO.isMemberLoginValid(loginMember);
+        }
+        catch (RuntimeException e){
+            MemberTestUtil.thenErrorMessageShouldBeEquals(NoRegisteredIDException.message, e.getMessage());
+        }
+    }
+
+    @Test
+    public void isMemberLoginValidFail_InvalidPassWordException(){
+        // Given
+        LoginMember loginMember = MemberTestUtil.givenLoginMember();
+        LoginMember getLoginMember = MemberTestUtil.givenLoginMember();
+        getLoginMember.setPassWord("Wrong PWD");
+        when(memberDB.getRegisteredLoginMember(loginMember)).thenReturn(getLoginMember);
+
+        try {
+            // When
+            memberDAO.isMemberLoginValid(loginMember);
+        }
+        catch (RuntimeException e){
+            MemberTestUtil.thenErrorMessageShouldBeEquals(InvalidPassWordException.message, e.getMessage());
+        }
+    }
+
+    @Test
+    public void updateMemberEntitySuccess(){
+        // Given
+        MemberEntity memberEntity = MemberTestUtil.givenMemberEntity();
+
+        // When
+        memberDAO.updateMemberEntity(memberEntity);
+
+        // Then
+        verify(memberDB, times(1)).removeMemberEntityByID(memberEntity.getId());
+        verify(memberDB, times(1)).registerMemberEntity(memberEntity);
+    }
+
+    @Test
+    public void deleteMemberSuccess(){
+        // Given
+        DeleteMember deleteMember = MemberTestUtil.givenDeleteMember();
+        when(memberDB.isDeleteMemberValid(deleteMember)).thenReturn(true);
+
+        // When
+        memberDAO.deleteMember(deleteMember);
+
+        // Then
+        verify(memberDB, times(1)).removeMemberEntityByID(deleteMember.getId());
+
+    }
+
+    @Test
+    public void deleteMemberFail_InvalidPassWordException(){
+        // Given
+        DeleteMember deleteMember = MemberTestUtil.givenDeleteMember();
+        when(memberDB.isDeleteMemberValid(deleteMember)).thenReturn(false);
+
+        try{
+            // When
+            memberDAO.deleteMember(deleteMember);
+        }
+        catch (RuntimeException e){
+            MemberTestUtil.thenErrorMessageShouldBeEquals(InvalidPassWordException.message, e.getMessage());
+        }
+
+    }
+
+    @Test
+    public void searchMemberEntitiesByMemberSuccess(){
+        // Given
+        Member member = MemberTestUtil.givenMember();
+        List<MemberEntity> memberEntities = new ArrayList<>();
+        when(memberDB.searchMemberEntitiesByMember(member)).thenReturn(memberEntities);
+
+        // When
+        List<MemberEntity> response = memberDAO.searchMemberEntitiesByMember(member);
+
+        // Then
+        assert response.equals(memberEntities);
+    }
 
 
 
